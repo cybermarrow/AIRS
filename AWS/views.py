e51573f84dcc46c11dbe2d39bb6d4cf27c210b7d
Lovlesh Malik <lmalik@lovleshs-mbp.corp.adobe.com>
2019-02-06 17:06:16 +0530

    Initial check in

---
+++ AWS/views.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import boto3
+
+import datetime
+
+def hello(request):
+    today = datetime.datetime.now().date()
+    return render(request,"hello.html",{"today" : today})
+
+def viewArticle(request, articleId):
+   text = "Displaying article Number : %s"%articleId
+   return HttpResponse(text)
+
+def aws_vpcs():
+    print "Returning list of vpcs now"
+    client = boto3.client('ec2', region_name='us-east-1')
+    response = client.describe_vpcs(
+        Filters=[
+            {
+                'state': 'available'
+            }
+        ]
+    )
+    resp = response['Vpcs']
+    print resp
